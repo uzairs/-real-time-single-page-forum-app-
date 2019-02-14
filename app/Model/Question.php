f6bad481e1d1deb8db2dcb28245abf6c7a6123e4
Uzair Durrani <uzairdurrani93@gmail.com>
2019-02-14 16:44:56 +0500

    Relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,41 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Category;
+use App\Model\User;
+use App\Model\Reply;
+
 
 class Question extends Model
 {
-    //
+
+
+   public function user() {
+   
+    //one to Many relationship
+    return $this->blongsTo(User::class); 
+  
+  }
+
+  public function replices()
+  {
+
+  //one many revers relationship
+   return $this->hasMany(Reply::class);
+
+  }
+
+  public function category()
+ 
+ {
+
+   //one to Many  relationship
+    return $this->blongsTo(Category::class);
+  
+  }
+
+
+
+
+
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -3,8 +3,36 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Model\Question;
+use App\Model\User;
+use App\Model\Like;
 class Reply extends Model
 {
-    //
+  public function user()
+  
+  {
+
+  return $this->blongsTo(User::class);
+  
+}
+
+  public function question()
+  
+  {
+
+     return $this->blongsTo(Question::class);
+
+
+  }
+
+  public function like(){
+
+
+    return $this->hasMany(Like::class);
+  }
+
+
+
+
+
 }
